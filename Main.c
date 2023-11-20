ffcba171cf8ee924f240b70ff6db39343f63fe1b
SuperKipipling <151564574+SuperKipipling@users.noreply.github.com>
2023-11-20 15:32:57 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,1 +1,21 @@
-hjhhgjgh
+#include <stdio.h>
+struct Data{
+    int dia;
+    int mes;
+    int ano;
+};
+struct Cliente{
+    char nome[50];
+    char telefone[15];
+};
+struct Animal{
+    char nomeanimal[50];
+    int especie;
+    char agressivo;
+    
+}
+struct serviço{
+    int identificador;
+    int serviço;
+    char pagamento;
+}
