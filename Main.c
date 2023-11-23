5db1a515a97e8fe12066e6785fd04aba0e169dd9
SuperKipipling <151564574+SuperKipipling@users.noreply.github.com>
2023-11-23 14:27:19 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 struct Data{
     int dia;
     int mes;
@@ -9,13 +10,17 @@ struct Cliente{
     char telefone[15];
 };
 struct Animal{
+    struct Cliente cliente;
     char nomeanimal[50];
     int especie;
+    struct Data dataanimal;
     char agressivo;
     
 };
 struct serviço{
     int identificador;
+    struct Data dataserviço;
+    struct Animal pet;
     int serviço;
     char pagamento;
 };
